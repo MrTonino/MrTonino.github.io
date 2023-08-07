4be79537914dd2b6ea3cb4fdb44891fd5bc3b816
Anthony <chidiebere.chuma@gmail.com>
2023-08-07 22:50:35 +0100

    Add Welcome component

---
+++ src/Welcome.js
@@ -0,0 +1,11 @@
+import React from "react";
+function Welcome(props) {
+  return (
+    <h1>
+      Welcome to {props.name}'s Portfolio Website! This project showcases my
+      skills, projects, and achievements as a web developer. It's a reflection
+      of my journey and growth in the world of web development.
+    </h1>
+  );
+}
+export default Welcome;
